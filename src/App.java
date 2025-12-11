cc98c616e09a5de23fc9f2f4bfabcba4bc3f84ad
Rodrigo <Rodrigo@Baby-Face>
2025-12-10 23:58:29 -0300

    Implementación de funciones App

--- src/App.java
+++ src/App.java
@@ -1,9 +1,57 @@
+import java.util.Scanner;
 
 public class App {
-
+	public Scanner s = new Scanner(System.in);
+	public Scanner t;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Lectura de Archivos
+		lecturaUsuarios();
+		lecturaRegistros();
+		lecturaNotas();
+		lecturaEstudiantes();
+		lecturaCursos();
+		lecturaCertificaciones();
+		lecturaAsignaturas_Certificaciones();
+		
+		
+		
 
+	}
+
+	private static void lecturaAsignaturas_Certificaciones() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void lecturaCertificaciones() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void lecturaCursos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void lecturaEstudiantes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void lecturaNotas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void lecturaRegistros() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void lecturaUsuarios() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
