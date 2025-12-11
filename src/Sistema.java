7ef25e9f990a4a8e425b40c264c1f442e9ae642d
Rodrigo <Rodrigo@Baby-Face>
2025-12-11 01:16:58 -0300

    Implemetación de sistemaImpl

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,6 +1,20 @@
 
 public interface Sistema {
 	
+	public void lectura_Usuario(String nombre, String contraseña, String rol, String infoAdicional);
+	public void lectura_Curso(String NCR, String nombre, int numSemestre, int cantCreditos, String area, String requisito);
+	public void lectura_Estudiante(String rut, String nombre, String carrera, int numSemestre, String correo, String contraseña);
+	public void lectura_Certificacion(String id, String nombre, String descripcion, String requisitos, String validez);
+	public void lectura_Registro(String estudiante, String certificacion, String fecha, String estado, String progreso);
+	public void lectura_Notas(String estudiante, String curso, double calificacion, String estado, String semestre);
+	public void asignacion_Certificacion(String id,String nrc);
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
