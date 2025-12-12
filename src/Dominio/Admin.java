8ea1c24a60e5e378adfc0b87f8a9f9227fdae599
Rodrigo <rodrigo.reyes@alumnos.ucn.cl>
2025-12-12 03:21:03 -0300

    Javadoc admin
    Added documentation for the Admin class and its constructor.

--- src/Dominio/Admin.java
+++ src/Dominio/Admin.java
@@ -1,7 +1,18 @@
 package Dominio;
 
+/**
+ * Representa a un usuario con rol de Administrador.
+ * Los administradores tienen acceso a funciones de gestión del sistema.
+ */
 public class Admin extends Usuario{
 
+	/**
+	 * Constructor para la clase Admin.
+	 *
+	 * @param identificacion El identificador único del administrador.
+	 * @param contraseña La contraseña para el acceso al sistema.
+	 * @param rol El rol del usuario, que debe ser "Admin".
+	 */
 	public Admin(String identificacion, String contraseña, String rol) {
 		super(identificacion, contraseña, rol);
 	}
